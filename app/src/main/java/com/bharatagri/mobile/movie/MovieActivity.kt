8b47f2a33ee6d663431de59c3e08874b69eb8995
Anil Kumar Jha <anilkumarjha1995@gmail.com>
2020-10-12 16:04:41 +0530

    Added navigation component from Jetpack

--- app/src/main/java/com/bharatagri/mobile/movie/MovieActivity.kt
+++ app/src/main/java/com/bharatagri/mobile/movie/MovieActivity.kt
@@ -1,21 +1,11 @@
 package com.bharatagri.mobile.movie
 
-import android.os.Bundle
 import com.bharatagri.mobile.R
 import com.bharatagri.mobile.base.BaseActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MovieActivity : BaseActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        openFragment()
-    }
 
     override fun getLayoutResourceId() = R.layout.activity_movie
-
-    private fun openFragment() {
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.fragContainerView, MoviesFragment()).commit()
-    }
 }
